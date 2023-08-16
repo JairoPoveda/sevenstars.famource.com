7be857a32202cf6b63a4322f466b11e3b4eb5692
JairoPoveda <jairopoveda2022@hotmail.com>
2023-08-16 20:54:45 +0800

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      transitionProperty: {
+        'height': 'height',
+        'spacing': 'margin, padding',
+      },
+      boxShadow: {
+        'menuShadow' : '0px 4px 7px 0px rgba(183, 199, 230, 0.3)',
+      }
+    },
   },
   plugins: [],
 }
